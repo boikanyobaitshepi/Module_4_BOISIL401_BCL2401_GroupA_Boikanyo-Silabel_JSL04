47ab40e27ec5e19d3dc5bbc0fa965db4aa9c6cb9
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-03-20 11:23:46 +0200

    changes

--- Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -18,21 +18,3 @@ console.log(temperatureSuitability);
 let drinkDecision = currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.';
 console.log(drinkDecision);
 
-
-// Exercise 1: Create a Numeric Literal Expression
-let daysAvailable = 7;
-console.log(`Number of days the special energy drink is available: ${daysAvailable}`);
-
-// Exercise 2: Combine String Literals
-let tagline = "Unleash" + " Your Potential!";
-console.log(`Tagline for the new energy drink: "${tagline}"`);
-
-// Exercise 3: Use Identifiers
-let hydrationDrink = "Electrolyte+";
-let energyDrink = "Caffeine Blast";
-let comboDrink = hydrationDrink + " & " + energyDrink;
-console.log(`Combined energy drink product: "${comboDrink}"`);
-
-// Exercise 4: Experiment with Grouping Operators
-let totalCost = (2 * 5) * 7;
-console.log(`Total cost of buying energy drinks for a week: $${totalCost}`);
