e3a2ca7a3a6ef987743e8bc442c8539189a3d203
boikanyobaitshepi <156679487+boikanyobaitshepi@users.noreply.github.com>
2024-03-20 13:36:58 +0200

    changes

--- Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
+++ Module_4_StudentNo_Classcode_Group_Name-Surname_JSL04-main/index.js
@@ -1,20 +1,19 @@
-// Challenge 1: Flavor Selection
-let flavorPreference = userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular';
-console.log(`Flavor preference: ${flavorPreference}`);
+// #### Challenge 1: Flavor Selection
+let userPreference = 'Sugar-free';
+console.log(`Preferred variety: ${userPreference === 'Sugar-free' ? 'Sugar-free' : 'Regular'}`);
 
-// Challenge 2: Stock Check
-let stockStatus = cansLeft < 5 ? 'Time to restock!' : 'We\'re stocked!';
-console.log(stockStatus);
+// #### Challenge 2: Stock Check
+let cansLeft = 3;
+console.log(`${cansLeft < 5 ? 'Time to restock!' : "We're stocked!"}`);
 
-// Challenge 3: Workout Intensity
-let energyLevel = heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!';
-console.log(energyLevel);
+// #### Challenge 3: Workout Intensity
+let heartRate = 95;
+console.log(`${heartRate < 100 ? 'Boost needed!' : 'Energy levels are high!'}`);
 
-// Challenge 4: Temperature Suitability
-let temperatureSuitability = currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!';
-console.log(temperatureSuitability);
+// #### Challenge 4: Temperature Suitability
+let currentTemp = 4;
+console.log(`${currentTemp <= 5 ? 'Chilled to perfection!' : 'Needs a cooler!'}`);
 
-// Challenge 5: Late Night Coding Session
-let drinkDecision = currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.';
-console.log(drinkDecision);
-
+// #### Challenge 5: Late Night Coding Session
+let currentHour = 22;
+console.log(`${currentHour >= 7 && currentHour < 24 ? 'Unleash the beast!' : 'Better stick to water.'}`);
